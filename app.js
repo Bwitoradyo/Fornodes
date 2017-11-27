dd2683da1efbb8652c87823d20c56a1c4f3a5018
Bwitoradyo <bambang.witoradyo@gmail.com>
2017-11-27 05:03:55 -0500

    Simplified Input With Yargs

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const fs = require("fs");
 const _ = require("lodash");
 
 const notes = require("./notes");
+
+console.log(process.argv);
+
+var command = process.argv[2];
+console.log("Command: ", command);
+
+console.log(process.argv);
+
+if(command === "add"){
+  console.log("Adding new note");
+}else if(command === "list"){
+  console.log("Listing all notes");
+}else if(command === "read"){
+  console.log("Fetching notes")
+}else if(command === "remove"){
+  console.log("Remove notes")
+}else{
+  console.log("Command not recognized!")
+}
