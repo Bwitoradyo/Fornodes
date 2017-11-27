3d39572fd2942d65220663f314514fa8f5367178
Bwitoradyo <bambang.witoradyo@gmail.com>
2017-11-26 21:25:24 -0500

    Restarting App With Nodemon

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ console.log("Starting app.");
 
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash")
 const notes = require("./notes");
 
-const res = notes.divideMe(2, 23.3);
-console.log(res);
+console.log(_.isString(true));
+console.log(_.isNumber(123));
+console.log(_.isString("Andrew"));
+
+var filteredArray = _.uniq(["Bambang", 1, "Bambang", 1, 2, 3, 4])
+console.log(filteredArray);
+
+//const res = notes.divideMe(2, 23.3);
+//console.log(res);
 //console.log("The result is: ", notes.addMe(4, 2));
 
 
