65d1b4878cae9537c81706ecb86d1d6818988ca9
Bwitoradyo <bambang.witoradyo@gmail.com>
2017-11-26 21:07:46 -0500

    Using 3rd Party Modules

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ console.log("Starting app.");
 
 const fs = require("fs");
 const os = require("os");
+const notes = require("./notes");
 
+const res = notes.divideMe(2, 23.3);
+console.log(res);
+//console.log("The result is: ", notes.addMe(4, 2));
+
+
+/*
 const user = os.userInfo();
 
 
-fs.appendFile("greetings.txt", `Hello ${user.username}!`, (err) =>{
+fs.appendFile("greetings.txt", `Hello ${user.username}! You are ${notes.age}`, (err) =>{
   if(err){
     console.log("Unable to write to file")
   }
 })
+*/
---
+++ notes.js
@@ -0,0 +1,13 @@
+console.log("Starting notes.js");
+
+module.exports.divideMe = (a, b) => {
+  console.log("The result of " + a + " : " + b + " is:");
+  return a/b;
+}
+
+/*
+module.exports.addNote= () => {
+  console.log("addNote");
+  return "New note";
+};
+*/
