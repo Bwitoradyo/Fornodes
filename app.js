4d36307ad544ea790240ceb99d6408cc99a36e5d
Bwitoradyo <bambang.witoradyo@gmail.com>
2017-11-27 04:46:27 -0500

    Getting Input From User

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
 console.log("Starting app.");
 
 const fs = require("fs");
-const os = require("os");
-const _ = require("lodash")
+const _ = require("lodash");
+
 const notes = require("./notes");
-
-console.log(_.isString(true));
-console.log(_.isNumber(123));
-console.log(_.isString("Andrew"));
-
-var filteredArray = _.uniq(["Bambang", 1, "Bambang", 1, 2, 3, 4])
-console.log(filteredArray);
-
-//const res = notes.divideMe(2, 23.3);
-//console.log(res);
-//console.log("The result is: ", notes.addMe(4, 2));
-
-
-/*
-const user = os.userInfo();
-
-
-fs.appendFile("greetings.txt", `Hello ${user.username}! You are ${notes.age}`, (err) =>{
-  if(err){
-    console.log("Unable to write to file")
-  }
-})
-*/
--- notes.js
+++ notes.js
@@ -1,13 +1,6 @@
 console.log("Starting notes.js");
 
-module.exports.divideMe = (a, b) => {
-  console.log("The result of " + a + " : " + b + " is:");
-  return a/b;
-}
-
-/*
 module.exports.addNote= () => {
   console.log("addNote");
   return "New note";
 };
-*/
